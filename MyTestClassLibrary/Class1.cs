be799e5bcada57a286ca5ee62b936a33a3589c8f
Pedja Kulezic <LEGRANDCRM\pedja>
2014-12-31 13:43:44 +1100

    Initial branch

---
+++ MyTestClassLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestClassLibrary
+{
+    public class Class1
+    {
+        public int MyProperty { get; set; }
+    }
+}
